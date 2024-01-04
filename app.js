f74dee30e662968fb05643557596ddc4b609e4dd
dev <devcsgrad@gmail.com>
2024-01-04 14:43:51 +0530

    "swagger docs added"

--- app.js
+++ app.js
@@ -21,17 +21,27 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+//Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 app.set('trust proxy', 1);
 app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000,
-    max: 100
-  })
+    rateLimiter({
+        windowMs: 15 * 60 * 1000,
+        max: 100
+    })
 );
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get('/', (req, res) => {
+    res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 
 // routes
@@ -44,12 +54,12 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-      await connectDB(process.env.MONGO_URI);
-      app.listen(port,console.log(`Server is listening on port ${port}...`));
-  } catch (error) { 
-    console.log(error);
-  }
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
