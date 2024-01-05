5761bdf96f74f8f32442d6c509a9c541ad1cef1f
dev <devcsgrad@gmail.com>
2024-01-05 15:54:47 +0530

    added delete user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,29 +1,37 @@
 const User = require('../models/User');
-const {StatusCodes} = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 
-const register = async(req,res)=>{
-    const user = await User.create({...req.body});
+const register = async (req, res) => {
+    const user = await User.create({ ...req.body });
     const token = user.createJWT();
-    res.status(StatusCodes.CREATED).json({user :{name:user.name},token});
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 }
 
-const login = async(req,res)=>{
-    const{email,password} = req.body;
-    if(!email || !password){
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
         throw new BadRequestError('Please Provide Name and Password');
     }
-    const user = await User.findOne({email});
-    if(!user){
+    const user = await User.findOne({ email });
+    if (!user) {
         throw new UnauthenticatedError('Invalid Credentials');
     }
     const isPasswordCorrect = await user.comparePassword(password);
-    if(!isPasswordCorrect){
+    if (!isPasswordCorrect) {
         throw new UnauthenticatedError('Invalid Credentials');
     }
     const token = user.createJWT();
-    res.status(StatusCodes.OK).json({user:{name:user.name},token});
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 }
 
-module.exports = {register,login}; 
+const deleteUser = async (req, res) => {
+    const { params: { email: email } } = req;
+    const user = await User.findOneAndDelete({ email: email });
+    if (!user) {
+        throw new NotFoundError(`User does not exist with email ${email}`);
+    }
+    res.status(StatusCodes.OK).json({ name: user.name, email: user.email });
+}
+module.exports = { register, login, deleteUser };
