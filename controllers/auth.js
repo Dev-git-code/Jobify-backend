9f868402bc989e43d182b4cd6e32213d0b94cb66
dev <devcsgrad@gmail.com>
2024-01-10 12:58:11 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,37 +1,44 @@
 const User = require('../models/User');
-const { StatusCodes } = require('http-status-codes');
-const { BadRequestError, UnauthenticatedError } = require('../errors');
+const Job = require('../models/Job');
+const {StatusCodes} = require('http-status-codes');
+const { BadRequestError, UnauthenticatedError, NotFoundError} = require('../errors');
 
 
-const register = async (req, res) => {
-    const user = await User.create({ ...req.body });
+
+const register = async(req,res)=>{
+    const user = await User.create({...req.body});
     const token = user.createJWT();
-    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+    res.status(StatusCodes.CREATED).json({user :{name:user.name},token});
 }
 
-const login = async (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
+const login = async(req,res)=>{
+    const{email,password} = req.body;
+    if(!email || !password){
         throw new BadRequestError('Please Provide Name and Password');
     }
-    const user = await User.findOne({ email });
-    if (!user) {
+    const user = await User.findOne({email});
+    if(!user){
         throw new UnauthenticatedError('Invalid Credentials');
     }
     const isPasswordCorrect = await user.comparePassword(password);
-    if (!isPasswordCorrect) {
+    if(!isPasswordCorrect){
         throw new UnauthenticatedError('Invalid Credentials');
     }
     const token = user.createJWT();
-    res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+    res.status(StatusCodes.OK).json({user:{name:user.name},token});
 }
 
-const deleteUser = async (req, res) => {
-    const { params: { email: email } } = req;
-    const user = await User.findOneAndDelete({ email: email });
-    if (!user) {
+const deleteUser = async(req,res)=>{
+    const {params:{email:email}} = req;
+    const user = await User.findOneAndDelete({email:email});
+    if(!user){
         throw new NotFoundError(`User does not exist with email ${email}`);
+    }else{      
+         const job = await Job.deleteMany({createdBy:user._id});
+         if(!job){
+            throw new NotFoundError(`No Jobs for the user exists ${email}`);
+         }
     }
-    res.status(StatusCodes.OK).json({ name: user.name, email: user.email });
+    res.status(StatusCodes.OK).json({name:user.name,email:user.email});
 }
-module.exports = { register, login, deleteUser };
+module.exports = {register,login,deleteUser}; 
